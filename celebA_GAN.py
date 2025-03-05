0b6849c7c4cc3d1a56da223a36c6f39f5fd0833b
Mahsa Agz <83410581+mahsaAgz@users.noreply.github.com>
2025-03-05 11:07:34 +0900

    Update celebA_GAN.py

--- celebA_GAN.py
+++ celebA_GAN.py
@@ -1,5 +1,7 @@
 # Do not add any other packages
 # Use these packages to write your code
+# Do not change
+
 import argparse
 import os
 import random
@@ -14,6 +16,8 @@ import numpy as np
 from PIL import Image
 
 # Transforming celeba dataset
+# Do not change
+
 def transform_fn(is_training):
     if is_training:
         return T.Compose([
@@ -42,6 +46,8 @@ def transform_fn(is_training):
 
 # Celeba dataset class
 # We use 10,000 images as train set and 1,000 images as evaluation set
+# Do not change
+
 class CelebADataset(Dataset):
 
     def __init__(self, args, is_train):
@@ -65,6 +71,15 @@ class CelebADataset(Dataset):
         image = self.transform(image)
         return image
 
+
+# Function that produces noise vector
+# The output is noise_vector and seed which is added with one
+# Use like below
+# noise, seed = generate_noise(batch_size, noise_size, seed)
+# You must use your this code when defining your noise vector
+# Otherwise, the result will be different from TA's
+# Do not change
+
 def generate_noise(batch_size, noise_size, seed):
     seed = seed + 1
     random.seed(seed)
@@ -75,7 +90,11 @@ def generate_noise(batch_size, noise_size, seed):
     return torch.randn(batch_size, noise_size) , seed
 
 
+
 def main():
+
+    # This code takes "seed", "image path" "epoch to stop" and "output path for the result" for argument
+    # Do not change
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default = 0)
@@ -86,6 +105,10 @@ def main():
 
     assert args.img_path is not None, 'image path should be specified'
     assert args.output_path is not None, 'output path should be specified'
+
+
+    # Fix the seed for reproducibilty
+    # Do not change
 
     seed = args.seed
     random.seed(seed)
@@ -103,18 +126,19 @@ def main():
     train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2)
     eval_dataloader = DataLoader(eval_dataset, batch_size=32, shuffle=False, num_workers=2)
 
+
+    ################################################################
+   
     class Generator(nn.Module):
       def __init__(self):
           super(Generator, self).__init__()
           self.main = nn.Sequential(
               nn.Linear(100, 256),
-              nn.LeakyReLU(0.1),
-              nn.Linear(256, 512),
-              nn.LeakyReLU(0.1),
-              nn.Linear(512, 64),
-              nn.LeakyReLU(0.1),
+              nn.LeakyReLU(0.2),
+              nn.Linear(256, 64),
+              nn.LeakyReLU(0.2),
               nn.Linear(64, 64),
-              nn.LeakyReLU(0.1),
+              nn.LeakyReLU(0.2),
               nn.Linear(64, 64 * 64 * 3),
               nn.Sigmoid(),
           )
@@ -149,56 +173,57 @@ def main():
             output = self.main(input)
             return output.view(-1, 1).squeeze(1)
     
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    netG = Generator().cuda()
-    netD = Discriminator().cuda()
-    criterion = nn.BCELoss()
-
-    # Create batch of latent vectors that we will use to visualize
-    #  the progression of the generator
-    fixed_noise = generate_noise(32, 100, seed)
-
-    # Establish convention for real and fake labels during training
-
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    netG = Generator().to(device)
+    netD = Discriminator().to(device)
 
     optimizerG = optim.AdamW(netG.parameters(), lr=0.0001)
     optimizerD = optim.AdamW(netD.parameters(), lr=0.0001)
 
-
+#############################
+    # fixed_noise= generate_noise(batch_size, 100,seed)[0].to(device)
     criterion = nn.BCELoss()
-    G_losses = []
-    D_losses = []
-    iters = 0
     total_loss=0
-    
-    # the total epoch is set to 5
+    ################################################################
 
+    # the total epoch is set to 5
+    # do not change
     for epoch in range(5):
         for batch_idx, data in enumerate(train_dataloader):
-    
+            ###################################################################
+            ## TODO : Train and evaluate your model
+            ## change below XXX into your loss variable on whole evaluation set
+            ####################################################################
+
+            ####################################################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z))) #
+            ###################################################
+            # train with real
             netD.zero_grad()
             netG.zero_grad() 
-            data = data.cuda()
+            data = data.to(device)
             batch_size = data.size(0)
-            real_label = torch.ones((batch_size,)).cuda()
+            label = torch.ones((batch_size,)).to(device)
             output = netD(data)
-            errD_real = criterion(output, real_label)
+            errD_real = criterion(output, label)
             D_x = output.mean().item()
 
             # train with fake
-            noise = generate_noise(batch_size, 100,seed)[0].cuda()
+            noise = generate_noise(batch_size, 100,seed)[0].to(device)
             fake = netG(noise)
-            fake_label = torch.zeros((batch_size,)).cuda()
+            label = torch.zeros((batch_size,)).to(device)
             output = netD(fake.detach())
-            errD_fake = criterion(output, fake_label)
+            errD_fake = criterion(output, label)
             D_G_z1 = output.mean().item()
 
             errD = errD_real + errD_fake
             errD.backward()
             optimizerD.step()
-
+            ########################################
+            # (2) Update G network: maximize log(D(G(z))) #
+            ########################################
             netG.zero_grad()
-            label = torch.ones((batch_size,)).cuda() 
+            label = torch.ones((batch_size,)).to(device) 
             # label should be 1 fake labels are real for generator cost
             # because it G wants to fool D
             output = netD(fake)
@@ -207,35 +232,41 @@ def main():
 
             errG.backward()
             optimizerG.step()
-            total_loss += errD.item() + errG.item()
+        print('[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+                % (epoch, n_epoch, errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-        eval_loss_t = 0.0
+        #save the output
+        # fake = netG(fixed_noise)
+        # training_progress_images_list = save_gif(training_progress_images_list, fake)  # Save fake image while training!
+
+        # # Check pointing for every epoch
+        # torch.save(netG.state_dict(), './checkpoint/netG_epoch_%d.pth' % (epoch))
+        # torch.save(netD.state_dict(), './checkpoint/netD_epoch_%d.pth' % (epoch))
+
+        total_loss += errD.item() + errG.item()
+
+        eval_loss = 0.0
         for batch_idx, eval_data in enumerate(eval_dataloader):
             batch_size = eval_data.size(0)
-            eval_data = eval_data.cuda()
+            eval_data = eval_data.to(device)
             output_real = netD(eval_data)
-            label_real = torch.ones((batch_size,)).cuda()
-            label_fake = torch.zeros((batch_size,)).cuda()
-        
-            noise = generate_noise(batch_size, 100,seed)[0].cuda()
+            label_real = torch.ones((batch_size,)).to(device)
+            D_loss=criterion (output_real,label_real)
+
+            noise = generate_noise(batch_size, 100,seed)[0].to(device)
             fake = netG(noise)
             output_fake = netD(fake)
+            label_fake = torch.zeros((batch_size,)).to(device)
             # errG = criterion(output_fake, label)
+            G_loss= criterion (output_fake,label_fake)
 
 
-            D_loss_real = criterion(output_real, label_real)
-            D_loss_fake = criterion(output_fake, label_fake)
-            D_loss = D_loss_real + D_loss_fake
-
-            G_loss= criterion (output_fake,label_real)
-           
-            eval_loss_t += D_loss + G_loss
-
-        eval_loss=eval_loss_t.item()
+            eval_loss += D_loss + G_loss
 
 
       # At argument's stop epoch, below code writes your evaluation loss (at certain epoch) to txt file
       # Then, it stops the for loop
+      # Do not change
         if epoch == args.stop_epoch:
             loss = round(eval_loss, 3)
             print(loss)
@@ -244,9 +275,11 @@ def main():
             script_name = os.path.basename(script_path)
             script_name = script_name[script_name.rfind("/")+1:]
             script_name = script_name[:script_name.rfind(".")]
+
             with open(os.path.join(args.output_path, 'result.txt'), 'a') as f:
                 f.write(f"{script_name}\t{str(loss)}\n")
-            break
+        break
 
 if __name__ == '__main__':
     main()
+
